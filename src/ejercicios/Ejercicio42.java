1316d035568bead03f80eef717d0451080c4041b
JorgeCastro13ppp <jlcastroc01@educarex.es>
2025-11-05 12:07:25 +0100

    Terminar 42,43+//

---
+++ src/ejercicios/Ejercicio42.java
@@ -0,0 +1,23 @@
+package ejercicios;
+
+/**
+ * Ejercicio 42 - Crear fichero XML con JAXB
+ * Asignatura: Acceso a Datos (2º DAM)
+ *
+ * Descripción:
+ * El programa solicita al usuario los datos de una librería (nombre, lugar, CP)
+ * y los de sus libros (título, autor, editorial, ISBN). 
+ * Luego genera un fichero XML con la estructura solicitada utilizando JAXB.
+ * 
+ * Autor: Jorge Leonardo Castro Chiluisa
+ * Fecha: 5 de Noviembre de 2025
+ */
+
+public class Ejercicio42 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
